796a23282c26f0a1d0502ed6b897b0cdb438d6bb
Abhishek Bhagwat <Abhishek.bhagwat1999@gmail.com>
2018-09-19 15:44:05 +0800

    Delete main.js

--- intuition-2018-gh-pages 2/app/js/main.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = () => {
-  const nav = document.querySelector('.navbar');
-  if (this.scrollY <= 250) nav.className = 'nav navbar navbar-expand-lg navbar-light fixed-top';
-
-  else nav.className = 'nav navbar navbar-expand-lg navbar-light fixed-top on-scroll';
-};
